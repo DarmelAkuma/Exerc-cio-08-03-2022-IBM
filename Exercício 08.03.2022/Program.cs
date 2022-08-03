3fcd9a89903f2815fba96585f064184289188cca
Mateus Melo Oliveira <mateus.melo@ibm.com>
2022-08-03 17:04:27 -0300

    Commit inicial

---
+++ Exercício 08.03.2022/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercício_08._03._2022 {
+    class Program {
+        static void Main(string[] args) {
+
+            int A, B, X;
+            Console.WriteLine();
+            A = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            B = int.Parse(Console.ReadLine());
+            X = A + B;
+            Console.WriteLine($" X = {X} ");         
+        }
+    }
+}
